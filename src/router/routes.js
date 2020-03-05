7221a9d6b834f11423fac7d9270a838e7026cd39
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-05 15:14:32 +0700

    assign newly added routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,10 @@ function page (path) {
 export default [
   { path: '/', name: 'home', component: page('Home.vue') },
   { path: '/articles/:id', name: 'articles.detail', component: page('ArticleDetail.vue') },
+
+  { path: '/infographics', name: 'infographics.list', component: page('InfographicList.vue') },
+  { path: '/infographics/:id', name: 'infographics.detail', component: page('InfographicDetail.vue') },
+
   { path: '/messages', name: 'messages', component: page('Messages.vue') },
   { path: '/messages/:id', name: 'messages.detail', component: page('MessageDetail.vue') },
   { path: '*', component: page('errors/404.vue') }
