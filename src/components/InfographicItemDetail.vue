5bd609cfd75b9323df65c900068d4e5d2eb76736
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-05 15:13:43 +0700

    remove unused title and subtitle from component

--- src/components/InfographicItemDetail.vue
+++ src/components/InfographicItemDetail.vue
@@ -1,15 +1,5 @@
 <template>
   <div class="rounded-lg bg-white shadow-lg overflow-hidden">
-    <div class="p-8">
-      <header class="flex items-center text-brand-blue-darkest">
-        <i class="mr-4 fa fa-image fa-2x relative opacity-25"></i>
-        <span class="font-bold uppercase text-base tracking-widest leading-none">Infografik</span>
-      </header>
-      <h3 class="mt-4 font-bold text-3xl text-gray-900 leading-tight">
-        {{infographic.title}}<br>
-        <small class="font-normal text-gray-600">{{infographic.subtitle}}</small>
-      </h3>
-    </div>
     <img v-for="(image, index) in images"
           :key="index"
           :src="image"
