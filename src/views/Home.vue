a932c2b0a5ead1f787d5be76e6cd534b26d01c21
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-06 09:45:28 +0700

    update number of response

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@
                 :
               </span>
               <span class="text-4xl lg:mt-2 text-brand-yellow-lighter">
-                154
+                122
               </span>
             </h4>
           </home-card>
